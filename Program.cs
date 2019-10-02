371210bb5e739c91da61067b4944acece061d90d
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-10-02 16:04:07 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace numcount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
